fa33c364e976b997ee5d8331c756412723d56cd3
Edbergarn <te16.emil.edberg@umea.nti.se>
2018-12-14 11:13:23 +0100

    2 active tomts

--- side.js
+++ side.js
@@ -50,23 +50,18 @@ let sliderSpeed = document.getElementById("id2");
 let tomte = document.getElementById("tomte");
 let p1X = 200;
 let p1Y = totalHeight - 125;
+let p2X= 1200
+let p2Y=totalHeight - 125;
 let v0Y;
 let v0X;
 let g;
 let v0;
 let alpha;
-
+let sy;
+let sx;
 let balls =[];
 
-
-let move = {
-	right: false,
-	left: false
-}
-
-rectangle(0,0,totalWidth,totalHeight, "green");
-context2D.drawImage(tomte, p1X, p1Y)
-
+//-------Irrelevant------------------------------------------
 function circle(x, y, r, color) 
 {
   context2D.fillStyle = color;
@@ -96,8 +91,23 @@ function clearScreen()//Clearar skärmen
 {
   context2D.clearRect(0, 0, totalWidth, totalHeight);
 }
+//------------------------------------------------------------
+
+
+let moveP1 = {//Move tomte 1
+	right: false,
+	left: false
+}
+let moveP2 = {//Move tomte 2
+	right: false,
+	left: false
+}
+
 function start()
 {
+rectangle(0,0,totalWidth,totalHeight, "green");//Ritar ut i början
+context2D.drawImage(tomte, p1X, p1Y);//Ritar ut i början
+context2D.drawImage(tomte, p2X, p2Y);//Ritar ut i början
   s2     = 0;
   g     = -9.82;
   v0    = sliderSpeed.value / 10;
@@ -107,25 +117,30 @@ function start()
   v0X   = v0*cos(alpha);
   v0Y   = v0*sin(alpha);
 }
+
+
 function drawBall(ballObj){
-	ballObj.sy = ballObj.v0Y*ballObj.t+(g*ballObj.t*ballObj.t)/2;
+	ballObj.sy = ballObj.v0Y*ballObj.t+(ballObj.g*ballObj.t*ballObj.t)/2;
 	ballObj.y = totalHeight - ballObj.sy;
 	ballObj.sx = ballObj.v0X*ballObj.t;
 	
-	circle((ballObj.sx + p1X + 32), (ballObj.y - 100), 10, "red");//Projektilen
+	circle((ballObj.sx + ballObj.x + 32), (ballObj.y - 100), 10, ballObj.color);//Projektilen
 	
 	ballObj.t += 0.1;
 }
-
 function update()
 {
   clearScreen(); //Clearar skärmen mellan varje frame
   rectangle(0,0,totalWidth,totalHeight,"green");//Min vackra gröna bakgrund
 
-  context2D.drawImage(tomte, p1X, p1Y);//Tomten
+  context2D.drawImage(tomte, p1X, p1Y);//Tomte 1
+  context2D.drawImage(tomte, p2X, p2Y);//Tomte 2
 
-  if (move.right && p1X < (totalWidth-64)) p1X = p1X + 3;// Smooth Movement i sidled för Player 1
-  if (move.left && p1X > 0) p1X = p1X - 3;// Smooth Movement i sidled för Player 1
+  if (moveP1.right && p1X < (totalWidth-64)) p1X = p1X + 3;// Smooth Movement i sidled för Player 1
+  if (moveP1.left && p1X > 0) p1X = p1X - 3;// Smooth Movement i sidled för Player 1
+
+  if (moveP2.right && p2X < (totalWidth-64)) p2X = p2X + 3;// Smooth Movement i sidled för Player 1
+  if (moveP2.left && p2X > 0) p2X = p2X - 3;// Smooth Movement i sidled för Player 1
 
   balls.forEach(element=> {
   	drawBall(element);
@@ -133,39 +148,77 @@ function update()
 }
 document.addEventListener("keydown", function(e){ // Smooth Movement i sidled för Player 1
 	switch(e.key){
-		case "ArrowRight":
-		move.right = true;
+		case "d":
+		moveP1.right = true;
 		break;
-		case "ArrowLeft":
-		move.left = true;
+		case "a":
+		moveP1.left = true;
 		break;
 	}
-})
+});
+document.addEventListener("keyup", function(e){ // Smooth Movement i sidled för Player 1
+	switch(e.key){
+		case "d":
+		moveP1.right = false;
+		break;
+		case "a":
+		moveP1.left = false;
+		break;
+	}
+});
+document.addEventListener("keydown", function(e){ // Smooth Movement i sidled för Player 1
+	switch(e.key){
+		case "ArrowRight":
+		moveP2.right = true;
+		break;
+		case "ArrowLeft":
+		moveP2.left = true;
+		break;
+	}
+});
 document.addEventListener("keyup", function(e){ // Smooth Movement i sidled för Player 1
 	switch(e.key){
 		case "ArrowRight":
-		move.right = false;
+		moveP2.right = false;
 		break;
 		case "ArrowLeft":
-		move.left = false;
+		moveP2.left = false;
 		break;
 	}
-})
+});
 
-window.onkeydown = function (e) { // SKjuter iväg en projektil
+document.addEventListener("keydown", function (e) { // Skjuter iväg en projektil
     var code = e.keyCode ? e.keyCode : e.which;
     if (code === 32) { //space key
 		let ball = {
 			x: p1X,
 			y: p1Y,
 			g: -9.8,
-			v0: sliderSpeed.value/10,
-			alpha: pi/(sliderAngle.value/10),
+			v0: (sliderSpeed.value / 10),
+			alpha: (pi/(sliderAngle.value / 10)),
 			t: 0,
 			v0Y: v0*cos(alpha),
-			v0X: v0*sin(alpha)
+			v0X: v0*sin(alpha),
+			color: "red"
 		}
 		balls.push(ball);
     }
-}
+});
+document.addEventListener("keydown", function (e) { // Skjuter iväg en projektil
+    var code = e.keyCode ? e.keyCode : e.which;
+    if (code === 16) { //shift key
+		let ball2 = {
+			x: p2X,
+			y: p2Y,
+			g: -9.8,
+			v0: (sliderSpeed.value / 10),
+			alpha: (pi/((sliderAngle.value /10))),
+			t: 0,
+			v0Y: v0*cos(alpha),
+			v0X: v0*sin(alpha) * -1,
+			color: "#FFFFFF"
+		}
+		balls.push(ball2);
+    }
+});
 window.requestAnimationFrame(start);
