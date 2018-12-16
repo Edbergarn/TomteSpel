4a06b29a12a9ed19f9ed90bdb3854598f33f5d75
Edbergarn <te16.emil.edberg@umea.nti.se>
2018-12-16 11:31:55 +0100

    fungerande spel med 2 spelare

--- side.js
+++ side.js
@@ -19,16 +19,6 @@ down  = 40;
 left  = 37;
 right = 39;
 space = 32;
-zero  = 48;
-one   = 49;
-two   = 50;
-three = 51;
-four  = 52;
-five  = 53;
-six   = 54;
-seven = 55;
-eight = 56;
-nine  = 57;
 w     = 87;
 a     = 65;
 s     = 83;
@@ -45,21 +35,34 @@ let totalHeight = canvas.height;
 let pi = Math.PI;
 let FPS = 60;
 let theAnimation = setInterval(update, 1000/FPS);//Mitt Drawgrej
-let sliderAngle = document.getElementById("id1");
-let sliderSpeed = document.getElementById("id2");
+let p1Angle;
+let p2Angle;
 let tomte = document.getElementById("tomte");
-let p1X = 200;
+let p1X = 200; // Tomte 1 startPos
 let p1Y = totalHeight - 125;
-let p2X= 1200
+let p2X= totalWidth-264; // Tomte 2 startPos
 let p2Y=totalHeight - 125;
-let v0Y;
-let v0X;
+let v0Y; // Vinklar
+let v0X; // Vinklar
 let g;
 let v0;
-let alpha;
-let sy;
-let sx;
-let balls =[];
+let sy; //Avstånd från marken
+let sx; // Avstånd från skjutpunkt i Xled
+let xPos; // Bollens X position
+let yPos; // Bollens Y position
+let balls =[]; // Array med bollar
+let p1HP = 100;
+let p2HP = 100;
+let p1Win = false;
+let p2Win = false;
+let ballSpeed = 100;
+let p1AimY;
+let p1AimX;
+let p1AimV0;
+let p1AimV0Y;
+let p1AimV0X;
+let p1AimSy;
+let p1AimSx;
 
 //-------Irrelevant------------------------------------------
 function circle(x, y, r, color) 
@@ -102,51 +105,163 @@ let moveP2 = {//Move tomte 2
 	right: false,
 	left: false
 }
+let aimP1 = { //Aim tomte 1
+	right: false,
+	left: false
+}
+let aimP2 = { //Aim tomte 2
+	right: false,
+	left: false
+}
 
 function start()
 {
 rectangle(0,0,totalWidth,totalHeight, "green");//Ritar ut i början
 context2D.drawImage(tomte, p1X, p1Y);//Ritar ut i början
 context2D.drawImage(tomte, p2X, p2Y);//Ritar ut i början
-  s2     = 0;
-  g     = -9.82;
-  v0    = sliderSpeed.value / 10;
-  alpha = pi/(sliderAngle.value / 10);
-  t     = 0;
-
-  v0X   = v0*cos(alpha);
-  v0Y   = v0*sin(alpha);
+  	g     = -9.82;
+  	g2     = -9.82;
+  	v0    = 60;
+  	v02 = 20;
+  	t     = 0;
+  	t2 = 0.3;
+  	sx = v0Y * t + g*t*t;
+	p2Alpha =((p2Angle/180)*pi);
+	p1Alpha =((p1Angle/180)*pi);
+	p1Angle = 20;
+	p2Angle = 20;
 }
 
 
 function drawBall(ballObj){
-	ballObj.sy = ballObj.v0Y*ballObj.t+(ballObj.g*ballObj.t*ballObj.t)/2;
-	ballObj.y = totalHeight - ballObj.sy;
+	if (ballObj.color == "white") {
+	ballObj.v0X = cos(ballObj.p2Alpha) * ballObj.v0 * -1;
+	ballObj.v0Y = sin(ballObj.p2Alpha) * ballObj.v0;
+	} else{
+	ballObj.v0X = cos(ballObj.p1Alpha) * ballObj.v0;
+	ballObj.v0Y = sin(ballObj.p1Alpha) * ballObj.v0;
+	}
+	ballObj.sy = ballObj.v0Y*ballObj.t+(ballObj.g*(ballObj.t*ballObj.t))/2;
+	ballObj.y = totalHeight - ballObj.sy - 100;
 	ballObj.sx = ballObj.v0X*ballObj.t;
+	ballObj.xPos = ballObj.sx + ballObj.x + 32;
+	ballObj.yPos = ballObj.y;
 	
-	circle((ballObj.sx + ballObj.x + 32), (ballObj.y - 100), 10, ballObj.color);//Projektilen
+	circle((ballObj.xPos), (ballObj.yPos), 10, ballObj.color);//Projektilen
+
 	
 	ballObj.t += 0.1;
-}
+	if (ballObj.color == "red") {
+		if (ballObj.xPos > p2X && ballObj.xPos < (p2X + tomte.width) && ballObj.yPos > p2Y && ballObj.yPos < (p2Y + 125)) {
+			balls.splice(balls.indexOf(ballObj), 1);
+			if (p2HP > 5) {
+				p2HP -= 6;
+			}else if (0 < p2HP && p2HP < 6) {
+				p2HP=0;
+			} else {
+				return p1Win = true;
+			}
+			let p2HPProcent = p2HP / 100;
+		}
+	}
+	if (ballObj.color == "white") {
+		if (ballObj.xPos > p1X && ballObj.xPos < (p1X + tomte.width) && ballObj.yPos > p1Y && ballObj.yPos < (p1Y + 125)) {
+			balls.splice(balls.indexOf(ballObj), 1);
+			if (p1HP > 5) {
+				p1HP -= 6;
+			}else if (0 < p1HP && p1HP < 6) {
+				p1HP=0;
+			} else {
+				return p2Win = true;
+			}
+			let p1HPProcent = p1HP / 100;
+		}
+	}
+	return p1HP, p2HP;
+} 
+
 function update()
 {
-  clearScreen(); //Clearar skärmen mellan varje frame
-  rectangle(0,0,totalWidth,totalHeight,"green");//Min vackra gröna bakgrund
+	clearScreen(); //Clearar skärmen mellan varje frame
+	rectangle(0,0,totalWidth,totalHeight,"green");//Min vackra gröna bakgrund
 
-  context2D.drawImage(tomte, p1X, p1Y);//Tomte 1
-  context2D.drawImage(tomte, p2X, p2Y);//Tomte 2
+	if (moveP1.right && p1X < (totalWidth - tomte.width)) p1X = p1X + 3;// Smooth Movement i sidled för Tomte 1
+	if (moveP1.left && p1X > 0) p1X = p1X - 3;// Smooth Movement i sidled för Tomte 1
 
-  if (moveP1.right && p1X < (totalWidth-64)) p1X = p1X + 3;// Smooth Movement i sidled för Player 1
-  if (moveP1.left && p1X > 0) p1X = p1X - 3;// Smooth Movement i sidled för Player 1
+	if (moveP2.right && p2X < (totalWidth - tomte.width)) p2X = p2X + 3;// Smooth Movement i sidled för Tomte 2
+	if (moveP2.left && p2X > 0) p2X = p2X - 3;// Smooth Movement i sidled för Tomte 2
 
-  if (moveP2.right && p2X < (totalWidth-64)) p2X = p2X + 3;// Smooth Movement i sidled för Player 1
-  if (moveP2.left && p2X > 0) p2X = p2X - 3;// Smooth Movement i sidled för Player 1
 
-  balls.forEach(element=> {
-  	drawBall(element);
-  });
+	//------Aim Tomte 1--------------------
+	if (aimP1.right && p1Angle > 0) {
+			p1Angle--;
+	}
+	if (aimP1.left && p1Angle < 180) {
+			p1Angle++;
+	}
+	//------Aim Tomte 2--------------------
+	if (aimP2.right && p2Angle > 0) {
+			p2Angle--;
+	}
+	if (aimP2.left && p2Angle < 180) {
+			p2Angle++;
+	}
+	//-------------------------------------
+	
+	context2D.drawImage(tomte, p1X, p1Y);//Tomte 1
+	context2D.drawImage(tomte, p2X, p2Y);//Tomte 2
+
+	let p1HPProcent = p1HP / 100;
+	let p2HPProcent = p2HP / 100;
+  	p1AimX = p1X;
+	p1AimV0 = ballSpeed;
+	p1Alpha = (((p1Angle - 90)/180)*pi);
+	p1AimV0Y = p1AimV0 * cos(p1Alpha);
+	p1AimV0X = p1AimV0 * sin(p1Alpha) * -1;
+	p1AimSy = p1AimV0Y*t2+(g2*t2*t2)/2;
+  	p1AimY = totalHeight - p1AimSy;
+  	p1AimSx = p1AimV0X*t2;
+
+  	p2AimX = p2X;
+	p2AimV0 = ballSpeed;
+	p2Alpha = (((p2Angle - 90)/180)*pi);
+	p2AimV0Y = p2AimV0 * cos(p2Alpha);
+	p2AimV0X = p2AimV0 * sin(p2Alpha);
+	p2AimSy = p2AimV0Y*t2+(g2*t2*t2)/2;
+  	p2AimY = totalHeight - p2AimSy;
+  	p2AimSx = p2AimV0X*t2;
+
+
+
+
+
+	balls.forEach(element=> {
+		drawBall(element);
+	});
+	context2D.strokeStyle = black;
+	context2D.lineWidth = 2;
+	context2D.strokeRect(p1X-10, p1Y-15, 84,14);
+	rectangle(p1X-8, p1Y-13, 80 * p1HPProcent, 10, red);
+	context2D.strokeStyle = black;
+	context2D.lineWidth = 2;
+	context2D.strokeRect(p2X-10, p2Y-15, 84,14);
+	rectangle(p2X-8, p2Y-13, 80* p2HPProcent, 10, red);
+
+  	circle(p1AimSx + p1AimX + 32, p1AimY -100, 5, "black");//Tomte 1 aim hjälp
+  	circle(p2AimSx + p2AimX + 32, p2AimY -100, 5, "black");//Tomte 1 aim hjälp
+
+	if (p1Win) {
+		context2D.font = "30px Arial";
+		context2D.fillText("Player 1 Wins!!!", 600, 335);
+		clearInterval(theAnimation);
+	}
+	if (p2Win) {
+		context2D.font = "30px Arial";
+		context2D.fillText("Player 2 Wins!!!", 600, 335);
+		clearInterval(theAnimation);
+	}
 }
-document.addEventListener("keydown", function(e){ // Smooth Movement i sidled för Player 1
+document.addEventListener("keydown", function(e){ // Smooth Movement i sidled för Tomte 1
 	switch(e.key){
 		case "d":
 		moveP1.right = true;
@@ -154,9 +269,27 @@ document.addEventListener("keydown", function(e){ // Smooth Movement i sidled f�
 		case "a":
 		moveP1.left = true;
 		break;
+		case "D":
+		moveP1.right = true;
+		break;
+		case "A":
+		moveP1.left = true;
+		break;
+		case "w":
+		aimP1.right = true;
+		break;
+		case "s":
+		aimP1.left = true;
+		break;
+		case "W":
+		aimP1.right = true;
+		break;
+		case "S":
+		aimP1.left = true;
+		break;
 	}
 });
-document.addEventListener("keyup", function(e){ // Smooth Movement i sidled för Player 1
+document.addEventListener("keyup", function(e){ // Smooth Movement i sidled för Tomte 1
 	switch(e.key){
 		case "d":
 		moveP1.right = false;
@@ -164,9 +297,27 @@ document.addEventListener("keyup", function(e){ // Smooth Movement i sidled för
 		case "a":
 		moveP1.left = false;
 		break;
+		case "D":
+		moveP1.right = false;
+		break;
+		case "A":
+		moveP1.left = false;
+		break;
+		case "w":
+		aimP1.right = false;
+		break;
+		case "s":
+		aimP1.left = false;
+		break;
+		case "W":
+		aimP1.right = false;
+		break;
+		case "S":
+		aimP1.left = false;
+		break;
 	}
 });
-document.addEventListener("keydown", function(e){ // Smooth Movement i sidled för Player 1
+document.addEventListener("keydown", function(e){ // Smooth Movement i sidled för Tomte 2
 	switch(e.key){
 		case "ArrowRight":
 		moveP2.right = true;
@@ -174,15 +325,27 @@ document.addEventListener("keydown", function(e){ // Smooth Movement i sidled f�
 		case "ArrowLeft":
 		moveP2.left = true;
 		break;
+		case "ArrowUp":
+		aimP2.right = true;
+		break;
+		case "ArrowDown":
+		aimP2.left = true;
+		break;
 	}
 });
-document.addEventListener("keyup", function(e){ // Smooth Movement i sidled för Player 1
+document.addEventListener("keyup", function(e){ // Smooth Movement i sidled för Tomte 2
 	switch(e.key){
 		case "ArrowRight":
 		moveP2.right = false;
 		break;
 		case "ArrowLeft":
 		moveP2.left = false;
+		break;
+		case "ArrowUp":
+		aimP2.right = false;
+		break;
+		case "ArrowDown":
+		aimP2.left = false;
 		break;
 	}
 });
@@ -193,12 +356,14 @@ document.addEventListener("keydown", function (e) { // Skjuter iväg en projekti
 		let ball = {
 			x: p1X,
 			y: p1Y,
+			p2X: p2X,
+			p2Y: p2Y,
 			g: -9.8,
-			v0: (sliderSpeed.value / 10),
-			alpha: (pi/(sliderAngle.value / 10)),
+			v0: (ballSpeed),
+			p1Alpha: ((p1Angle / 180)* pi),
 			t: 0,
-			v0Y: v0*cos(alpha),
-			v0X: v0*sin(alpha),
+			v0Y: v0*cos(p1Alpha),
+			v0X: v0*sin(p1Alpha) * -1,
 			color: "red"
 		}
 		balls.push(ball);
@@ -206,17 +371,19 @@ document.addEventListener("keydown", function (e) { // Skjuter iväg en projekti
 });
 document.addEventListener("keydown", function (e) { // Skjuter iväg en projektil
     var code = e.keyCode ? e.keyCode : e.which;
-    if (code === 16) { //shift key
+    if (code === 13) { //shift key
 		let ball2 = {
 			x: p2X,
 			y: p2Y,
+			p1X: p1X,
+			p1Y: p1Y,
 			g: -9.8,
-			v0: (sliderSpeed.value / 10),
-			alpha: (pi/((sliderAngle.value /10))),
+			v0: (ballSpeed),
+			p2Alpha: ((p2Angle/180)*pi),
 			t: 0,
-			v0Y: v0*cos(alpha),
-			v0X: v0*sin(alpha) * -1,
-			color: "#FFFFFF"
+			v0Y: v0 * cos(p2Alpha),
+			v0X: v0 * sin(p2Alpha) * -1,
+			color: "white"
 		}
 		balls.push(ball2);
     }
